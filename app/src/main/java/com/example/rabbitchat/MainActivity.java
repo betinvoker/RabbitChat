d6d3ea91f90e924bd3c04f70dbbcc775b8e13efc
betinvoker <betinvoker1797@gmail.com>
2020-06-13 23:46:28 +0300

    Bug fixed
    The bug is fixed. Implemented the ability to send messages to the server

--- app/src/main/java/com/example/rabbitchat/MainActivity.java
+++ app/src/main/java/com/example/rabbitchat/MainActivity.java
@@ -1,22 +1,96 @@
 package com.example.rabbitchat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static int MAX_MESSANGE_LENGTH = 200;
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference myRef = database.getReference("messages");
 
+    ArrayList<String> messages = new ArrayList<>();
 
+    EditText mEditTextMessage;
+    Button mSendButton;
+    RecyclerView mMessagesRecycler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSendButton = findViewById(R.id.send_message_b);
+        mEditTextMessage = findViewById(R.id.message_input);
+        mMessagesRecycler = findViewById(R.id.messages_recycler);
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = mEditTextMessage.getText().toString();
+
+                if(msg.equals("")){
+                    Toast.makeText(getApplicationContext(), "Введите сообщение", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(msg.length() > MAX_MESSANGE_LENGTH){
+                    Toast.makeText(getApplicationContext(), "Вы привысили лимит символов в сообщении(" + MAX_MESSANGE_LENGTH + ")", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                myRef.push().setValue(msg);
+
+                mEditTextMessage.setText("");
+            }
+        });
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String msg = dataSnapshot.getValue(String.class);
+                messages.add(msg);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
