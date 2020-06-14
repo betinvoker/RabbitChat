ed5198a1f8cc5b6fcb0218f22b28011784abc8c8
betinvoker <betinvoker1797@gmail.com>
2020-06-14 19:10:57 +0300

    No data output
    Error. Database data not output

--- app/src/main/java/com/example/rabbitchat/MainActivity.java
+++ app/src/main/java/com/example/rabbitchat/MainActivity.java
@@ -1,106 +1,113 @@
 package com.example.rabbitchat;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
+import android.text.format.DateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int MAX_MESSANGE_LENGTH = 200;
+    private static int SIGN_IN_CODE = 1;
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("messages");
+    private RelativeLayout activity_main;
+    private FloatingActionButton sendBtn;
 
-    ArrayList<String> messages = new ArrayList<>();
-
-    EditText mEditTextMessage;
-    Button mSendButton;
-    RecyclerView mMessagesRecycler;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SIGN_IN_CODE) {
+            if(requestCode == RESULT_OK) {
+                Snackbar.make(activity_main,"Вы авторизованы", Snackbar.LENGTH_LONG).show();
+                displayAllMessages();
+            }
+            else {
+                Snackbar.make(activity_main,"Вы авторизованы", Snackbar.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSendButton = findViewById(R.id.send_message_b);
-        mEditTextMessage = findViewById(R.id.message_input);
-        mMessagesRecycler = findViewById(R.id.messages_recycler);
-
-        mMessagesRecycler.setLayoutManager(new LinearLayoutManager(this));
-
-        final DataAdapter dataAdapter = new DataAdapter(this, messages);
-
-        mMessagesRecycler.setAdapter(dataAdapter);
-
-        mSendButton.setOnClickListener(new View.OnClickListener() {
+        activity_main = findViewById(R.id.activity_main);
+        sendBtn = findViewById(R.id.btnSend);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String msg = mEditTextMessage.getText().toString();
+                EditText textField = findViewById(R.id.messageField);
 
-                if(msg.equals("")){
-                    Toast.makeText(getApplicationContext(), "Введите сообщение", Toast.LENGTH_SHORT).show();
+                if(textField.getText().toString() == "") {
                     return;
                 }
 
-                if(msg.length() > MAX_MESSANGE_LENGTH){
-                    Toast.makeText(getApplicationContext(), "Вы привысили лимит символов в сообщении(" + MAX_MESSANGE_LENGTH + ")", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                myRef.push().setValue(msg);
-
-                mEditTextMessage.setText("");
+                FirebaseDatabase.getInstance().getReference().push().setValue(
+                        new Message(
+                                FirebaseAuth.getInstance().getCurrentUser().getEmail(),
+                                textField.getText().toString()
+                        )
+                );
+                textField.setText("");
             }
         });
 
-        myRef.addChildEventListener(new ChildEventListener() {
+        //Пользователь не авторизован
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE);
+        } else {
+            Snackbar.make(activity_main, "Вы авторизованы", Snackbar.LENGTH_LONG).show();
+            displayAllMessages();
+        }
+
+        displayAllMessages();
+
+    }
+
+    private void displayAllMessages() {
+        ListView lestOfMessages = findViewById(R.id.list_of_messages);
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("RabbitChat");
+        FirebaseListOptions<Message> options = new FirebaseListOptions.Builder<Message>().setQuery(query, Message.class).setLayout(R.layout.item_message).build();
+
+        FirebaseListAdapter<Message> adapter = new FirebaseListAdapter<Message>(options) {
             @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                String msg = dataSnapshot.getValue(String.class);
-                messages.add(msg);
-                dataAdapter.notifyDataSetChanged();
-                mMessagesRecycler.smoothScrollToPosition(messages.size());
-            }
+            protected void populateView(View v, Message model, int position) {
+                TextView msg_user, msg_text, msg_time;
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                msg_user = (TextView)v.findViewById(R.id.message_user);
+                msg_text = (TextView)v.findViewById(R.id.message_text);
+                msg_time = (TextView)v.findViewById(R.id.message_time);
 
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                msg_user.setText(model.getUserName());
+                msg_text.setText(model.getTextMessage());
+                msg_time.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
 
             }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        };
+        lestOfMessages.setAdapter(adapter);
     }
 }
